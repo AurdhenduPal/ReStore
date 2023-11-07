9f386af50a312e9dce241fc92eef40aefcc9eff3
Aurdhendu Pal <aurdhendup85@gmail.com>
2023-11-07 10:58:44 +0530

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext StoreContext)
+        {
+            _storeContext = StoreContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _storeContext.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+    }
+}
